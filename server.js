a03a4e64b10947417c67dea09439c18ebc350a3a
John Gantner <jayrgee@fastmail.fm>
2016-07-29 15:35:28 +1000

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -3,12 +3,13 @@
 
 // init project
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// create application/json parser
+var jsonParser = bodyParser.json();
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -16,27 +17,13 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/public/index.html');
 });
 
-app.get("/api", function (request, response) {
-  console.log("blah");
+app.post("/api", jsonParser, function (req, res) {
+
+    if (!req.body) return res.sendStatus(400);
+
+    console.log(req.body);
+    res.sendStatus(200);
 });
-
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-  ];
 
 // listen for requests :)
 listener = app.listen(process.env.PORT, function () {
